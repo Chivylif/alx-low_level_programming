c5923d142186988eddd8f96e33a8e229af30a647
Osigwe Chidozie <chivy4gud@gmail.com>
2021-09-06 07:24:26 -0700

    if, else if and while statements

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * main: Prints all alphabets except q and e
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		if (x != 'q' && x != 'e')
+		{
+			putchar(x);
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
