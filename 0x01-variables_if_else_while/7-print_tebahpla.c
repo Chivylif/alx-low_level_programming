28223849d580557fd1c815eb4409600afc0295c2
Osigwe Chidozie <chivy4gud@gmail.com>
2021-09-08 05:30:18 -0700

    if, else if and while statements

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ * main: print all alphabets in reverse order
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char x;
+
+	for (x = 'z'; x >= 'a'; x--)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
