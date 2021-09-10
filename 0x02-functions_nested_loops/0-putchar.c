074f1e8319b178ec52a7e82dc282b3943a7aa382
Osigwe Chidozie <chivy4gud@gmail.com>
2021-09-10 04:18:21 -0700

    Commit message for 0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry Point
+ * main: Prints _putchar
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char x[] = "_putchar";
+
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(x[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
