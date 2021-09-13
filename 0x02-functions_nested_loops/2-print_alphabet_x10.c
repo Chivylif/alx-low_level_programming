d28eaabad851e9e7d5e00b56a1a00cb7ce944dfc
Osigwe Chidozie <chivy4gud@gmail.com>
2021-09-13 16:15:20 +0100

    Commit message for 0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints alpbabets 10x
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char = x;
+
+	while (count <= 9)
+	{
+		for (x >= 'a'; x <= 'z'; x++)
+		{
+			_putchar(x);
+		}
+		_putchar('\n');
+		count = count + 1;
+	}
+}
