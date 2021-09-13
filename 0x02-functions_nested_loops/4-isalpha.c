8c82443f78126784f844801547373234ef3d7824
Osigwe Chidozie <chivy4gud@gmail.com>
2021-09-13 08:41:56 -0700

    Commit message for 0x02-functions_nested_loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,12 +9,12 @@
 
 int _isalpha(int c)
 {
-        if (c > 64 && c <= 122)
-        {
-                return (1);
-        }
-        else
-        {
-                return (0);
-        }
+	if (c > 64 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
