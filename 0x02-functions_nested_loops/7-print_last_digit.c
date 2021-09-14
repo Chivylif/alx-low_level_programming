f048d59edcc3fc9769a572aeaa82419f43be2f82
Osigwe Chidozie <chivy4gud@gmail.com>
2021-09-14 05:22:17 -0700

    Commit message for 0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints the last digit of a number
+ * @x: Number to passed as an arguement
+ *
+ * Return: Return x
+ */
+
+int print_last_digit(int x)
+{
+	int n;
+
+	n = (x % 10);
+	return (n);
+}
