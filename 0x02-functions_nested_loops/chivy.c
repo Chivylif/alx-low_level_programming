e583d46385aa1627af4905f56ef08ca6a6791afd
Osigwe Chidozie <chivy4gud@gmail.com>
2021-09-14 16:19:40 +0100

    Commit message for 0x02-functions_nested_loops

--- 0x02-functions_nested_loops/chivy.c
+++ 0x02-functions_nested_loops/chivy.c
@@ -6,23 +6,36 @@
  * Return: Return the 9 times table
  */
 
-void times_table(void)
-{
-	int i, j;
+void times_table(void)                                                                                                          
+{                                                                                                                               
+        int i, j;                                                                                                               
+                                                                                                                                
+        for (i = 0; i < 10; i++)                                                                                                
+        {                                                                                                                       
+                _putchar('0');                                                                                                  
+                                                                                                                                
+                for (j = 1; j < 10; j++)                                                                                        
+                {                                                                                                               
+                        int result;                                                                                             
+                                                                                                                                
+                        result = i * j;                                                                                         
+                        if (result > 9)                                                                                         
+                        {    
+                                _putchar(',');                                                                                  
+                                _putchar(' ');                                                                                  
+                                _putchar((result / 10) + '0');                                                                  
+                                _putchar((result % 10) + '0');                                                                  
+                                continue;                                                                                                    
+                        else                                                                                                    
+                        {                                                                                                       
+                                _putchar(',');                                                                                  
+                                _putchar(' ');                                                                                  
+                                _putchar(' ');
+				_putchar(result + '0');
 
-	for (i = 0; i < 10; i++)
-	{
-		for (j = 0; j < 10; j++)
-		{
-			int result;
-
-			result = i * j;
-			_putchar(result + '0');
-			if (j == 9)
-				continue;
-			_putchar(',');
-			_putchar(' ');
+                        }
 		}
 		_putchar('\n');
 	}
 }
+                        
