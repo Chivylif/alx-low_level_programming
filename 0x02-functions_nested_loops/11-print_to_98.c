0c267aada3a7396a5eb216fe9aace350819e2664
Osigwe Chidozie <chivy4gud@gmail.com>
2021-09-16 12:10:00 +0100

    Commit message for 0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_to_98 - Prints all the natural numbers from n to 98
+ * @n: the starting number
+ *
+ * Return: Nothing.
+ */
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n <= 98)
+		{
+			_putchar(n + '0');
+			if (n == 98)
+				continue;
+			_putchar(',');
+			_putchar(' ');
+			n++;
+		}
+	}
+	if (n > 98)
+	{
+		while (n >= 98)
+		{
+			_putchar(n + '0');
+			if (n == 98)
+				continue;
+			_putchar(',');
+			_putchar(' ');
+			n--;
+		}
+	}
+}
