68400fef6bdd32c797c3d8cc50d2bde776299401
Osigwe Chidozie <chivy4gud@gmail.com>
2021-09-16 11:45:52 +0100

    Commit message for 0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds two intergers
+ * @a: one of the intergers
+ * @b: the other interger
+ *
+ * Return: Always 0.
+ */
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
