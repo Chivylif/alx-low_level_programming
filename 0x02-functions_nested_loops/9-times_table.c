f1eb39427b9e2011a2860e382c841db1c17eff9b
Osigwe Chidozie <chivy4gud@gmail.com>
2021-09-16 03:27:05 -0700

    Commit message for 0x02-functions_nested_loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,36 +6,37 @@
  * Return: Return the 9 times table
  */
 
-void times_table(void)
-{
-	int rone, cone, d;
+void times_table(void)                                                                                                          
+{                                                                                                                               
+        int i, j;                                                                                                               
+                                                                                                                                
+        for (i = 0; i < 10; i++)                                                                                                
+        {                                                                                                                       
+                _putchar('0');                                                                                                  
+                                                                                                                                
+                for (j = 1; j < 10; j++)                                                                                        
+                {                                                                                                               
+                        int result;                                                                                             
+                                                                                                                                
+                        result = i * j;                                                                                         
+                        if (result > 9)                                                                                         
+                        {    
+                                _putchar(',');                                                                                  
+                                _putchar(' ');                                                                                  
+                                _putchar((result / 10) + '0');                                                                  
+                                _putchar((result % 10) + '0');                                                                  
+                                continue;
+			}				
+			else                                                                                                    
+                        {                                                                                                       
+                                _putchar(',');                                                                                  
+                                _putchar(' ');                                                                                  
+                                _putchar(' ');
+				_putchar(result + '0');
 
-	for (rone = 0; rone <= 9; rone++)
-	{
-		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
-		for (cone = 1; cone <= 9; cone++)
-		{
-			d = (rone * cone);
-			if ((d / 10) > 0)
-			{
-				_putchar((d / 10) + '0');
-			}
-			else
-			{
-				_putchar(' ');
-			}
-			_putchar((d % 10) + '0');
-
-			if (cone < 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+                        }
 		}
 		_putchar('\n');
 	}
-
 }
-
+                        
