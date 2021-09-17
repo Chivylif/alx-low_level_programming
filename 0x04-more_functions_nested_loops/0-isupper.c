8449ea6e8f3466cc19eed1c7829c0786c9be28bf
Osigwe Chidozie <chivy4gud@gmail.com>
2021-09-17 14:54:11 +0100

    more nested loops projects

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if a character is in upper case
+ * @n: character to be checked
+ *
+ * Return: Returns 1 or 0.
+ */
+
+int _isupper(int n)
+{
+	if (n <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
