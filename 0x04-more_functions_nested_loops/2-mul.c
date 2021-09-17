3e9034578e8fd5940d22abca0ca49e01518b2814
Osigwe Chidozie <chivy4gud@gmail.com>
2021-09-17 15:23:47 +0100

    more nested loops projects

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two intergers
+ * @a: First interger
+ *
+ * @b: Second interger
+ *
+ * Return: Return result.
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
