30fe009087c1d74b335e81d4854f77eddf1c975e
Osigwe Chidozie <chivy4gud@gmail.com>
2021-09-17 15:55:09 +0100

    more nested loops projects

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints number from 0 - 9 without printing 2 & 4
+ *
+ * Return: Always 0.
+ */
+
+void print_most_numbers(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		if (x != 2 && x != 4)
+		{
+			_putchar(x + '0');
+		}
+		x++;
+	}
+	_putchar('\n');
+}
+			
