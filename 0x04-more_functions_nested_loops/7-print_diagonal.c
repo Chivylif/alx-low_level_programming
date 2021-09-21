b1d9dc7657065964b8b9fa3d6f9dd2d4cb039205
Osigwe Chidozie <chivy4gud@gmail.com>
2021-09-21 02:56:52 -0700

    more nested loops projects

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,7 +8,7 @@
 
 void print_diagonal(int n)
 {
-	int x;
+	int x, y;
 
 	if (n <= 0)
 	{
@@ -22,7 +22,7 @@ void print_diagonal(int n)
 			{
 				_putchar(' ');
 			}
-			_putchar('\');
+			_putchar('\\');
 			_putchar('\n');
 		}
 	}
