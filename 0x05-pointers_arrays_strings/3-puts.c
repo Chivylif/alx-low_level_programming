8f20d5cd0abcd849bd97285c31f6abe5d0713729
Osigwe Chidozie <chivy4gud@gmail.com>
2021-09-21 16:16:54 +0100

    0x05-pointers_arrays_strings projects

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string to stdoutput
+ * @str: String to be printed
+ *
+ */
+
+void _puts(char *str)
+{
+	int x;
+
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		_putchar(str[x]);
+	}
+	_putchar('\n');
+}
