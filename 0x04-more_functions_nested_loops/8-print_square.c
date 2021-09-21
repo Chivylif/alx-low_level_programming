b6987dc56418960dadcd8da2d41cd9496750c351
Osigwe Chidozie <chivy4gud@gmail.com>
2021-09-21 11:11:08 +0100

    more nested loops projects

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square - Prints square of a given size
+ * @size: the given size of the square to be printed
+ *
+ */
+
+void print_square(int size)
+{
+	int x;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
+
