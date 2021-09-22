b5b164d447b403d33345c43717850d65382764e7
Osigwe Chidozie <chivy4gud@gmail.com>
2021-09-22 11:27:18 +0100

    0x05-pointers_arrays_strings projects

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character starting from 0 index No
+ * @str: Passed arguement String
+ *
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i+2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
