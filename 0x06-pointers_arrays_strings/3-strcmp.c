2567967f7f49ba8206ce8f495e41d33b4b145066
Osigwe Chidozie <chivy4gud@gmail.com>
2021-09-23 12:21:10 +0100

    0x06-pointers_arrays_strings Projects

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two strings
+ * @s1: first string
+i * @s2: second string
+ *
+ * Return: return a positive, a negative integer or 0
+ *
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+
+	return (*s1 - *s2);
+}
