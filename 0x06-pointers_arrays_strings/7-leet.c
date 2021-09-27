c807034fd246287e03f3fd9651eda940dace8def
Osigwe Chidozie <chivy4gud@gmail.com>
2021-09-27 13:31:11 -0700

    0x06-pointers_arrays_strings Projects

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,16 +11,16 @@ char *leet(char *str)
 {
 	int i, j = 0;
 
-	char str2[8] = {'O', 'L', '?', 'E', 'A'; '?', '?', 'T'};
+	char str2[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
 	for (i = 0; str[i]; i++)
 	{
-		for (j = 0; str2[j] < 8; j++)
+		for (j = 0; j < 8; j++)
 		{
 			if (str[i] == str2[j] ||
 			str[i] - 32 == str2[j])
 			{
-				str[i] = j;
+				str[i] = j + '0';
 			}
 		}
 	}
