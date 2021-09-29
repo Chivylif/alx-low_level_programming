7433cb9d600bab2cde21cbcfc23ccbd220e45ec4
Osigwe Chidozie <chivy4gud@gmail.com>
2021-09-29 03:30:53 -0700

    0x07-pointers_arrays_strings projects

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,17 +12,23 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int len = 0, i, j;
+	int len = 0, i, j, x;
 
 	for (i = 0; accept[i] != '\0'; i++)
 	{
+		x = 0;
+
 		for (j = 0; s[j] != '\0'; j++)
 		{
 			if (accept[i] == s[j])
 			{
 				len++;
+				x = 1;
 			}
 		}
+		if (x == 0)
+			return (len);			}
 	}
+
 	return (len);
 }
