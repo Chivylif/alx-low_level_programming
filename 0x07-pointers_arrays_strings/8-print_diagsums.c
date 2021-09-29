c717a5904c30cb3df17809971bb80a79c5320c04
Osigwe Chidozie <chivy4gud@gmail.com>
2021-09-29 08:46:09 -0700

    0x07-pointers_arrays_strings projects

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -13,9 +13,10 @@
 void print_diagsums(int *a, int size)
 {
 	int i, j, sum = 0, sum2 = 0;
-	
+
 	j = size - 1;
-	for (i = 0; i < (size * size); i = i + size + 1,j = (size - 1) + j)
+	for (i = 0; i < (size * size);
+		i = i + size + 1, j = (size - 1) + j)
 	{
 		sum = sum + a[i];
 		sum2 = sum2 + a[j];
