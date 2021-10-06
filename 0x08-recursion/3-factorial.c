93389b6f9c2caf219dfe8058c77be14bfbad7944
Osigwe Chidozie <chivy4gud@gmail.com>
2021-10-06 16:40:13 +0100

    0x08-recursion projects

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * factorial - prints the factorial of a given number n
+ * @n: The given number
+ *
+ * Returns: A number
+ */
+
+int factorial(int n)
+{
+	int x = 0;
+
+	if (n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		x = n * factorial(n - 1);
+		return (x);
+	}
+}
