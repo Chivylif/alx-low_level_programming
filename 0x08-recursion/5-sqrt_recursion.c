dd1ea9d24d5bb8254ab95c4c2db361ecb4087c38
Osigwe Chidozie <chivy4gud@gmail.com>
2021-10-07 04:39:18 -0700

    0x08-recursion projects

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,7 +2,7 @@
 int helper(int x, int y);
 
 /**
- * _int_sqrt_recursion - prints  the natural square root of a number
+ * _sqrt_recursion - prints  the natural square root of a number
  * @n: int input
  *
  * Return: the natural sqaure root
@@ -12,6 +12,14 @@ int _sqrt_recursion(int n)
 {
 	return (helper(n, 1));
 }
+
+/**
+ * helper - checks if a number is a perfect square
+ * @x: Number to be checked
+ * @y: counter
+ *
+ * Return: Returns the perfect squre root of x
+ */
 
 int helper(int x, int y)
 {
