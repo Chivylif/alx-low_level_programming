7350861c16488114f78fe81f44a01cd14cac1bc5
Osigwe Chidozie <chivy4gud@gmail.com>
2021-10-12 12:52:29 +0100

    0x0A-argc_argv comiits

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name
+ * @argc: number of command typed
+ * @argv: array containing the commands typed
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	argc = 0;
+	printf("s%\n", argv[argc]);
+	return (0);
+}
