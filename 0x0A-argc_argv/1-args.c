52114d7a2fb26e011bec3cbb762933cef27b2212
Osigwe Chidozie <chivy4gud@gmail.com>
2021-10-12 13:01:03 +0100

    0x0A-argc_argv comiits

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the command passed on the cmd
+ * @argc: number of command typed
+ * @argv: array containing the commands typed
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, count;
+
+	count = 0;
+	i = 0;
+	while (i < argc)
+	{
+		count++;
+		i++;
+	}
+	return (count);
+}
