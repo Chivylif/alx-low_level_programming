1ec541524a64416b9519b7c154f8c865525755b5
Osigwe Chidozie <chivy4gud@gmail.com>
2021-10-12 13:54:13 +0100

    0x0A-argc_argv comiits

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that prints all the arguments passed into it
+ *
+ * @argc: the number of command line arguments
+ * @argv: the array containing the program command line arguments
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int index;
+
+	index = 0;
+	while (index < argc)
+	{
+		printf("%s\n", argv[argc]);
+		index++;
+	}
+}
