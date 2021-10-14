4a714abc73debdce519aa4bb3aa1e65ee36c0969
Osigwe Chidozie <chivy4gud@gmail.com>
2021-10-14 16:14:00 +0100

    0x0B-malloc_free projects

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,31 +11,33 @@
 
 int **alloc_grid(int width, int height)
 {
-	int **arr;
-	int i, j;
+	int **iarray;
+	int i, n;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	arr = malloc(sizeof(int) * height);
+	iarray = malloc(sizeof(int) * height);
 
-	if (arr == NULL)
-		free(arr);
+	if (iarray == NULL)
+	{
+		free(iarray);
 		return (NULL);
+	}
 
 	for (i = 0; i < height; i++)
 	{
-		arr[i] = malloc(sizeof(int) * width);
-		if (arr == NULL)
-			free(arr);
-			return (NULL);
-
-		for (j = 0; j < width; j++)
+		iarray[i] = malloc(sizeof(int) * width);
+		if (iarray == NULL)
 		{
-			arr[i][j] = 0;
+			free(iarray);
+			return (NULL);
+		}
+
+		for (n = 0; n < width; n++)
+		{
+			iarray[i][n] = 0;
 		}
 	}
-	return (arr);
+	return (iarray);
 }
-
-	
