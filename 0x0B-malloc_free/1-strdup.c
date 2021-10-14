0ccbd7b33864a9ebe47674daacd66f9685a7b80d
Osigwe Chidozie <chivy4gud@gmail.com>
2021-10-14 10:36:40 +0100

    0x0B-malloc_free projects

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strdup - duplicate to new memory space location
+ * @str: char
+ * Return: A pointer
+ */
+
+char *_strdup(char *str)
+{
+	char *newstr;
+
+	int i, len, j;
+
+	if (str = NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (*str++)
+	{
+		i++;
+	}
+	len = i;
+	newstr = malloc(len * sizeof(char));
+	for (j = 0; j < len; j++)
+	{
+		newstr[j] = str[j];
+	}
+	if (newstr = NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		return (newstr);
+	}
+}
