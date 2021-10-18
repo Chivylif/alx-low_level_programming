a6d1232d5e6911c803aca76e897fc13450e96d96
Osigwe Chidozie <chivy4gud@gmail.com>
2021-10-18 10:39:59 +0100

    0x0C-more_malloc_free projects

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,6 +13,7 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	void *str;
 	char *ptr_copy, *filler;
 	unsigned int index;
 
@@ -21,12 +22,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (ptr == NULL)
 	{
-		ptr = malloc(new_size);
+		str = malloc(new_size);
 
-		if (ptr == NULL)
+		if (str == NULL)
 			return (NULL);
 
-		return (ptr);
+		return (str);
 	}
 
 	if (new_size == 0 && ptr != NULL)
