36b36abd4b63c3121ff14ea57d0f65e45d569756
Osigwe Chidozie <chivy4gud@gmail.com>
2021-10-19 14:39:12 +0100

    0x0F-function_pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,35 +11,37 @@
  * Return: Always 0.(On Success)
  */
 
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int num1, num2;
-	char *opss;
+	char *operator;
 
+	/* Error if arguments are not 4 with the executable */
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	num1 = atoi(argv[1]);
-	opss = argv[2];
-	num2 = atoi(argv[3]);
+	num1 = atoi(argv[1]); /*if argument is string convert to number*/
+	num2 = atoi(argv[3]); /* as stated above */
+	operator = argv[2];
 
-	if (get_op_func(opss) == NULL || op[1] != '\0')
+	/* Error if operator is not null or a 2d array '\*' */
+	if (get_op_func(operator) == NULL || operator[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((*opss == '/' && num2 == 0) ||
-	    (*opss == '%' && num2 == 0))
+	/* Error if user tries to divide / (47) or % (37) by 0*/
+	if ((*operator == 47 || *operator == 37) && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	printf("%d\n", get_op_func(opss)(num1, num2));
+	printf("%d\n", get_op_func(operator)(num1, num2));
 
 	return (0);
 }
