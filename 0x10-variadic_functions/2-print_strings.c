c811510dfdcb3d39941aa076ea78f2cc7434991c
Osigwe Chidozie <chivy4gud@gmail.com>
2021-10-20 14:57:17 +0100

    0x10-variadic_functions

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,36 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - Entry Point
+ * @separator: comma space
+ * @n: number of elements
+ * Return: void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ptr;
+	unsigned int i;
+	char *str;
+
+	if (separator == NULL)
+		return;
+
+	va_start(ptr, n);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(ptr, char *);
+		if (str == NULL)
+			printf("%s", "nil");
+		else
+			printf("%s", str);
+		if (i != (n - 1))
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ptr);
+}
+
+
