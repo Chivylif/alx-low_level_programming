20a27cc5840488184afe60ca9ee2fc7a64d57f45
Osigwe Chidozie <chivy4gud@gmail.com>
2021-10-20 14:34:38 +0100

    0x10-variadic_functions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -23,7 +23,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(ptr, int));
+		printf("%d", va_arg(ptr, const unsigned int));
 		if (i != (n - 1))
 			printf("%s", separator);
 	}
