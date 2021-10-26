967858d2c7577f3a0a909d8a3aaff08643bc568b
Osigwe Chidozie <chivy4gud@gmail.com>
2021-10-26 16:00:49 +0100

    0x14-bit_manipulation projects

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,6 +5,7 @@ unsigned int binary_to_uint(const char *b);
 #include <stdio.h>
 #include <stdlib.h>
 void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
 
 
 #endif
