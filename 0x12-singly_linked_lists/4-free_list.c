1b056ae4c65ad6212d0da807df07f06c91a6a9c6
Osigwe Chidozie <chivy4gud@gmail.com>
2021-11-12 15:58:32 +0100

    0x12-singly_linked_lists projects

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a linked list
+ * @head: list_t list to be freed
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	/**
+	 * while current head is not NULL
+	 * set head as the next node and
+	 * free the current node str then
+	 * free current node.
+	*/
+
+	while ((current = head) != NULL)
+	{
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
+}
