3a82dd323d6c22831efd6fb7a0e5d853411eac3c
Osigwe Chidozie <chivy4gud@gmail.com>
2021-11-15 10:34:01 +0100

    0x13-more_singly_linked_lists project commits

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -23,7 +23,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	if (*head == NULL)
 		*head = new_node;
-		return (new_node);
+		return (*head);
 
 	while (temp != NULL)
 		temp = temp->next;
