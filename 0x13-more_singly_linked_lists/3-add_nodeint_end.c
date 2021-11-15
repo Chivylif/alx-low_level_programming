c78c74375d95cc286787008cf7c0b746342991d4
Osigwe Chidozie <chivy4gud@gmail.com>
2021-11-15 10:25:18 +0100

    0x13-more_singly_linked_lists project commits

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -13,13 +13,13 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new_node, *temp;
 
-	if (*head == NULL)
-		*head = new_node;
-		return (new_node);
-
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
+
+	if (*head == NULL)
+		*head = new_node;
+		return (new_node);
 
 	new_node->n = n;
 	new_node->next = NULL;
